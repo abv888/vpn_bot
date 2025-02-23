001e5f9890cbea5eea5bbb2619d734aa510f1e61
Bagrat Arutyunov <arutyunovbagrat@gmail.com>
2025-02-23 11:50:46 +0300

    fix env

--- src/vpn_bot.py
+++ src/vpn_bot.py
@@ -1,6 +1,7 @@
 import asyncio
 import logging
 from os import getenv
+from pathlib import Path
 
 import yookassa
 from aiogram import Bot, Dispatcher
@@ -10,10 +11,23 @@ from db.models import init_db
 from handlers import router
 from middleware.vpn import VPNMiddleware
 
-load_dotenv(find_dotenv())
-
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
+
+BASE_DIR = Path(__file__).resolve().parent
+
+env_path =Path('.env')
+
+logger.info(f"File exists: {env_path.exists()}")
+
+load_dotenv(env_path)
+
+logger.info("Checking environment variables:")
+logger.info(f"DB_USER: {'Set' if getenv('DB_USER') else 'Not set'}")
+logger.info(f"DB_PASSWORD: {'Set' if getenv('DB_PASSWORD') else 'Not set'}")
+logger.info(f"VPN: {'Set' if getenv('VPN') else 'Not set'}")
+
+
 
 bot = Bot(token=getenv("BOT_TOKEN"))
 dp = Dispatcher()
